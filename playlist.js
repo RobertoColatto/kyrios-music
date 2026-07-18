d1dd9c2d954eb3479eae936f8da0c7bf552d2e61
Roberto Colatto <roberto.colatto@unesp.br>
2026-07-18 17:57:22 -0300

    add files

---
+++ playlist.js
@@ -0,0 +1,23 @@
+const playlist = [
+
+{
+title:"Música 1",
+file:"ALL OF ME instrumental (violino) - John Legend - música para casamento.mp3"
+},
+
+{
+title:"Música 2",
+file:"Música encontro de casais.mp3"
+},
+
+{
+title:"kkk",
+file:"musica3.mp3"
+},
+
+{
+title:"Música 4",
+file:"musica4.mp3"
+}
+
+];
---
+++ script.js
@@ -0,0 +1,142 @@
+// ===============================
+// COLOQUE SUAS MÚSICAS AQUI
+// ===============================
+
+console.log(playlist);
+
+// ===============================
+
+const audio = document.getElementById("audio");
+const musicName = document.getElementById("musicName");
+const playBtn = document.getElementById("playBtn");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progressContainer");
+const current = document.getElementById("current");
+const duration = document.getElementById("duration");
+const volume = document.getElementById("volume");
+const playlistDiv = document.getElementById("playlist");
+
+let currentMusic = 0;
+
+function loadMusic(index){
+
+audio.src = playlist[index].file;
+musicName.innerHTML = playlist[index].title;
+
+document.querySelectorAll(".playlist div").forEach((item,i)=>{
+item.classList.toggle("active",i===index);
+});
+
+}
+
+function playPause(){
+
+if(audio.paused){
+audio.play();
+playBtn.innerHTML="⏸";
+}else{
+audio.pause();
+playBtn.innerHTML="▶";
+}
+
+}
+
+function next(){
+
+currentMusic++;
+
+if(currentMusic>=playlist.length)
+currentMusic=0;
+
+loadMusic(currentMusic);
+audio.play();
+playBtn.innerHTML="⏸";
+
+}
+
+function previous(){
+
+currentMusic--;
+
+if(currentMusic<0)
+currentMusic=playlist.length-1;
+
+loadMusic(currentMusic);
+audio.play();
+playBtn.innerHTML="⏸";
+
+}
+
+audio.addEventListener("ended",next);
+
+audio.addEventListener("timeupdate",()=>{
+
+const percent=(audio.currentTime/audio.duration)*100;
+progress.style.width=percent+"%";
+
+current.innerHTML=format(audio.currentTime);
+duration.innerHTML=format(audio.duration);
+
+});
+
+function format(sec){
+
+if(isNaN(sec)) return "0:00";
+
+let min=Math.floor(sec/60);
+let s=Math.floor(sec%60);
+
+if(s<10) s="0"+s;
+
+return min+":"+s;
+
+}
+
+progressContainer.addEventListener("click",(e)=>{
+
+const width=progressContainer.clientWidth;
+const click=e.offsetX;
+
+audio.currentTime=(click/width)*audio.duration;
+
+});
+
+volume.addEventListener("input",()=>{
+
+audio.volume=volume.value;
+
+});
+
+playlist.forEach((music,index)=>{
+
+const item=document.createElement("div");
+
+item.innerHTML=music.title;
+
+item.onclick=()=>{
+
+currentMusic=index;
+loadMusic(index);
+audio.play();
+playBtn.innerHTML="⏸";
+
+};
+
+playlistDiv.appendChild(item);
+
+});
+
+loadMusic(currentMusic);
+
+//função de busca
+function searchMusic(playlist, searchStr) {
+    for(const item of playlist) {
+        for(const key in item) {
+            if(key == "title" && item[key].includes(searchStr)) {
+                console.log(item[key]);
+            }
+        }
+    }
+}
+
+searchMusic(playlist, "Músca")
