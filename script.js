7338cfe7261c35313faa1a8e62dbea148e2dd85f
Roberto Colatto <roberto.colatto@unesp.br>
2026-07-18 22:58:18 -0300

    ignore accents

--- script.js
+++ script.js
@@ -20,50 +20,50 @@ let currentMusic = 0;
 
 function loadMusic(index){
 
-audio.src = playlist[index].file;
-musicName.innerHTML = playlist[index].title;
+    audio.src = playlist[index].file;
+    musicName.innerHTML = playlist[index].title;
 
-document.querySelectorAll(".playlist div").forEach((item,i)=>{
-item.classList.toggle("active",i===index);
-});
+    document.querySelectorAll(".playlist div").forEach((item,i)=>{
+        item.classList.toggle("active",i===index);
+    });
 
 }
 
 function playPause(){
 
-if(audio.paused){
-audio.play();
-playBtn.innerHTML="⏸";
-}else{
-audio.pause();
-playBtn.innerHTML="▶";
-}
+    if(audio.paused){
+        audio.play();
+        playBtn.innerHTML="⏸";
+    }else{
+        audio.pause();
+        playBtn.innerHTML="▶";
+    }
 
 }
 
 function next(){
 
-currentMusic++;
+    currentMusic++;
 
-if(currentMusic>=playlist.length)
-currentMusic=0;
+    if(currentMusic>=playlist.length)
+        currentMusic=0;
 
-loadMusic(currentMusic);
-audio.play();
-playBtn.innerHTML="⏸";
+    loadMusic(currentMusic);
+    audio.play();
+    playBtn.innerHTML="⏸";
 
 }
 
 function previous(){
 
-currentMusic--;
+    currentMusic--;
 
-if(currentMusic<0)
-currentMusic=playlist.length-1;
+    if(currentMusic<0)
+        currentMusic=playlist.length-1;
 
-loadMusic(currentMusic);
-audio.play();
-playBtn.innerHTML="⏸";
+    loadMusic(currentMusic);
+    audio.play();
+    playBtn.innerHTML="⏸";
 
 }
 
@@ -71,72 +71,83 @@ audio.addEventListener("ended",next);
 
 audio.addEventListener("timeupdate",()=>{
 
-const percent=(audio.currentTime/audio.duration)*100;
-progress.style.width=percent+"%";
+    const percent=(audio.currentTime/audio.duration)*100;
+    progress.style.width=percent+"%";
 
-current.innerHTML=format(audio.currentTime);
-duration.innerHTML=format(audio.duration);
+    current.innerHTML=format(audio.currentTime);
+    duration.innerHTML=format(audio.duration);
 
 });
 
 function format(sec){
 
-if(isNaN(sec)) return "0:00";
+    if(isNaN(sec)) return "0:00";
 
-let min=Math.floor(sec/60);
-let s=Math.floor(sec%60);
+    let min=Math.floor(sec/60);
+    let s=Math.floor(sec%60);
 
-if(s<10) s="0"+s;
+    if(s<10) s="0"+s;
 
-return min+":"+s;
+    return min+":"+s;
 
 }
 
 progressContainer.addEventListener("click",(e)=>{
 
-const width=progressContainer.clientWidth;
-const click=e.offsetX;
+    const width=progressContainer.clientWidth;
+    const click=e.offsetX;
 
-audio.currentTime=(click/width)*audio.duration;
+    audio.currentTime=(click/width)*audio.duration;
 
 });
 
 volume.addEventListener("input",()=>{
 
-audio.volume=volume.value;
+    audio.volume=volume.value;
 
 });
 
 playlist.forEach((music,index)=>{
 
-const item=document.createElement("div");
+    const item=document.createElement("div");
 
-item.innerHTML=music.title;
+    item.innerHTML=music.title;
 
-item.onclick=()=>{
+    item.onclick=()=>{
 
-currentMusic=index;
-loadMusic(index);
-audio.play();
-playBtn.innerHTML="⏸";
+        currentMusic=index;
+        loadMusic(index);
+        audio.play();
+        playBtn.innerHTML="⏸";
 
-};
+    };
 
-playlistDiv.appendChild(item);
+    playlistDiv.appendChild(item);
 
 });
 
 loadMusic(currentMusic);
 
+//função auxiliar pra ignorar acentos
+function matchIgnoringAccents(str1, str2) {
+    const clean = (s) => s.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    return clean(str1).includes(clean(str2));
+}
+
 //função de busca
 function searchMusic(playlist, searchStr) {
+    const results = []
     for(const item of playlist) {
         for(const key in item) {
-            if(key == "title" && item[key].includes(searchStr)) {
-                console.log(item[key]);
+            //if(key == "title" && item[key].toLowerCase().includes(searchStr.toLowerCase())) {
+            if(key == "title" && matchIgnoringAccents(item[key], searchStr)) {
+                //console.log(item[key]);
+                results.push(item[key]);
             }
         }
     }
+    return results
 }
 
-searchMusic(playlist, "Digno")
+r = searchMusic(playlist, "")
+console.log(r)
